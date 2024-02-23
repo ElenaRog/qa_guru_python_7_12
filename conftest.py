bab5bab3f8a2ecf646505d34f7a99c3fa9400f1c
Elena Rogozina <q.www.ta@gmail.com>
2024-02-23 03:43:02 +0300

    test for local server

--- conftest.py
+++ conftest.py
@@ -9,6 +9,8 @@ from utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
+    from dotenv import load_dotenv
+    load_dotenv()
     browser_version = "100.0"
     options = Options()
     selenoid_capabilities = {
@@ -21,7 +23,8 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        #command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"http://192.168.0.17:4444/wd/hub",
         options=options
     )
     browser.config.driver = driver
